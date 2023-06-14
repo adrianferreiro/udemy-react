ad9a8450787f2457f57df78fa881b3e7c4b6e11c
Adrian <adriferreiro.88@gmail.com>
2023-06-14 08:09:57 -0300

    89 - Archivo de barril o indice
    Usamos para reducir líneas en las importaciones

---
+++ 02-gif-expert-app/src/components/index.js
@@ -0,0 +1,5 @@
+//Archivo de barril o índice
+
+export * from './AddCategory';//exporta todo lo que Add category exporta que en éste caso ese un componente
+export * from './GifGrid';
+export * from './GifItem';
