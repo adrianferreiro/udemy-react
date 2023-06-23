974eaeaf2b2779940af4c09344bb5e0883a1c79f
Adrian <adriferreiro.88@gmail.com>
2023-06-23 10:54:42 -0300

    122.- useEffect - precauciones

--- 03-hook-app/src/02-UseEffect/Message.jsx
+++ 03-hook-app/src/02-UseEffect/Message.jsx
@@ -1,10 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
+    const [coords, setCoords] = useState({x:0, y: 0})
 
   useEffect(() => {
     // first = cuerpo
-    console.log('Mensaje montado');
+    // console.log('Mensaje montado');
+    // vamos a crear un listener cuando se monta el componente
+    // const onMouseMove = (event) =>{
+    const onMouseMove = ({x,y}) =>{//desestructuramos x e y del event
+        const coords = { x, y };
+        // console.log(coords);
+        setCoords(coords);
+    }
+    // window.addEventListener('mousemove', (event)=>{
+    //     console.log(event.x, event.y);
+    // });
+    window.addEventListener('mousemove', onMouseMove);
     
     
     return () => {
@@ -12,13 +24,16 @@ export const Message = () => {
         //en éste ej lo que hicimos fue mostrar un mensaje cuando el campo de texto usuario
         //coincidía con "strider2" entonces el mensaje aparecía o desaparecía, es decir
         //se montaba o desmontaba
-        console.log('Mensaje desmontado');
+        // console.log('Mensaje desmontado');
+        window.removeEventListener('mousemove', onMouseMove);
+
     }
   }, []);
   
   return (
     <>
         <h3>Usuario ya existe</h3>
+        { JSON.stringify( coords ) }
     </>
     )
 }
