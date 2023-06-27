e46aa2aff506162edda4b0416e9ca25b756bf8ac
Adrian <adriferreiro.88@gmail.com>
2023-06-27 09:48:33 -0300

    132.- memorizamos el componente pero usando useMemo

--- 03-hook-app/src/main.jsx
+++ 03-hook-app/src/main.jsx
@@ -1,7 +1,8 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Memorize } from './06-memos/Memorize';
+// import { Memorize } from './06-memos/Memorize';
+import { MemoHook } from './06-memos/MemoHook';
 // import { FormWithCustomForm } from './02-UseEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
@@ -13,7 +14,7 @@ import { Memorize } from './06-memos/Memorize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <Memorize/>
+  <MemoHook/>
   // </React.StrictMode>,
 )
 // https://api.breakingbadquotes.xyz/v1/quotes/
