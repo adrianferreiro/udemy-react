64a1f3a40f4b27a260b32b854a0606384d7438ad
Adrian <adriferreiro.88@gmail.com>
2023-06-27 08:39:13 -0300

    130.- useLayoutEffect - Calcular el tamaño de un contenedor despues de que se randeriza

--- 03-hook-app/src/03-examples/Quotes.jsx
+++ 03-hook-app/src/03-examples/Quotes.jsx
@@ -1,11 +1,30 @@
+import { useLayoutEffect, useRef, useState } from "react"
+
 export const Quotes = ({quote, author}) => {
+
+  const pRef = useRef();
+
+  const [boxSize, setBoxSize] = useState({width: 0, height: 0});
+
+  useLayoutEffect(() => {
+  //vamos a obtener el tamaño de la caja después de que se randerizó
+  const {width, height} = pRef.current.getBoundingClientRect();
+  // console.log({width, height});
+    setBoxSize(width,height);
+  }, [quote])
     
   return (
-        <blockquote className="blockquote text-end">
-            <p className="mb-1"> {quote} </p>
+    <>
+        <blockquote 
+          className="blockquote text-end"
+          style={{display: 'flex', width: boxSize.width, height: boxSize.height}}
+          >
+            <p ref={pRef} className="mb-1"> {quote} </p>
             <footer className="blockquote-footer">
             {author}
             </footer>
         </blockquote>
+        <code>{JSON.stringify(boxSize)}</code>
+    </>
    )
 }
--- 03-hook-app/src/main.jsx
+++ 03-hook-app/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { FormWithCustomForm } from './02-UseEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 // import { SimpleForm } from './02-UseEffect/SimpleForm';
 // import { CounterWithCustomHook } from './01-UseState/CounterWithCustomHook';
 // import { CounterApp } from './01-UseState/CounterApp';
@@ -11,7 +12,7 @@ import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <FocusScreen/>
+  <Layout/>
   // </React.StrictMode>,
 )
 // https://api.breakingbadquotes.xyz/v1/quotes/
