544fc8e54a319ff50f875fee23c0bdc01106ca35
Adrian <adriferreiro.88@gmail.com>
2023-06-27 12:44:02 -0300

    141.- Idea general de un reducer

--- 03-hook-app/src/main.jsx
+++ 03-hook-app/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { CallBackHook } from './06-memos/CallBackHook';
 import { Padre } from './07-tarea-memo/Padre';
+import './08-useReducer/intro-reducer';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { FormWithCustomForm } from './02-UseEffect/FormWithCustomHook';
@@ -14,9 +15,9 @@ import { Padre } from './07-tarea-memo/Padre';
 // import { CounterApp } from './01-UseState/CounterApp';
 // import { HooksApp } from './HooksApp';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Padre/>
-  // </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <Padre/>
+//   // </React.StrictMode>,
+// )
 // https://api.breakingbadquotes.xyz/v1/quotes/
