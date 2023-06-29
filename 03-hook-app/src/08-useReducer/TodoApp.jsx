387b2522750a773a51634b169519c9135dc435ff
Adrian <adriferreiro.88@gmail.com>
2023-06-29 13:29:45 -0300

    148.- Borrar un TODO

--- 03-hook-app/src/08-useReducer/TodoApp.jsx
+++ 03-hook-app/src/08-useReducer/TodoApp.jsx
@@ -40,6 +40,13 @@ export const TodoApp = () => {
         dispatch(action);
     }
 
+    const handleDeleteTodo = (id) =>{
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id,
+        });
+    }
+
   return (
     <>
         <h1>TodoApp 10, <small>pendientes: 2</small></h1>
@@ -48,7 +55,7 @@ export const TodoApp = () => {
         <div className="row">
             <div className="col-7">
                 {/*  */}
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
                 {/*  */}
             </div>
 
--- 03-hook-app/src/08-useReducer/TodoList.jsx
+++ 03-hook-app/src/08-useReducer/TodoList.jsx
@@ -1,12 +1,15 @@
 import { TodoItem } from "./TodoItem"
 
-export const TodoList = ({todos = []}) => {
+export const TodoList = ({todos = [], onDeleteTodo}) => {
+  
+
+  
   return (
     <ul className="list-group">
         {
             todos.map(todo => (
             // eslint-disable-next-line react/jsx-key
-            <TodoItem key={todo.id} todo={todo} />   
+            <TodoItem key={todo.id} todo={todo} onDeleteTodo={ onDeleteTodo } />   
             ))
         }
     </ul>
