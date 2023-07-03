3f1754d1f608b01db3ca17c90150b6b177042121
Adrian <adriferreiro.88@gmail.com>
2023-07-03 12:38:10 -0300

    161.- useContext - Fin sección 11

--- 03-hook-app/src/09-useContext/HomePage.jsx
+++ 03-hook-app/src/09-useContext/HomePage.jsx
@@ -1,8 +1,19 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext"
+
 export const HomePage = () => {
+
+    const {user} = useContext(UserContext);
+
     return (
       <>
-          <h1>HomePage</h1>
+          {/* user?.name ... imprime el nombre solamente si existe */}
+          <h1>HomePage <small>{user?.name}</small></h1>
           <hr />
+          <pre>
+            {JSON.stringify(user, null, 3)} 
+            {/* null de replace 3 posiciones */}
+          </pre>
       </>
       )
   }
--- 03-hook-app/src/09-useContext/context/UserProvider.jsx
+++ 03-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
 
-const user = {
-    id: 123,
-    name: 'Fernando Herrera',
-    email: 'adrian@gmail.com'
-}
+// const user = {
+//     id: 123,
+//     name: 'Fernando Herrera',
+//     email: 'adrian@gmail.com'
+// }
 
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState();
+  
   return (
-    <UserContext.Provider value={{hola: 'Mundo', user: user}} >
+    // <UserContext.Provider value={{hola: 'Mundo', user: user}} >
+    <UserContext.Provider value={{ user, setUser }} >
         {/* randerizamos los childrens que vienen en las props */}
         {children} 
     </UserContext.Provider>
