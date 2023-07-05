49b6eb7b6748a845c8d9f30161008bd06b47eca0
Adrian <adriferreiro.88@gmail.com>
2023-07-05 11:37:40 -0300

    192.- Crear segundo Router - Separamos las rutas para diferenciar el login de las demas pages

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,20 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { DcPage, MarvelPage } from "../heroes";
+import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
-import {Navbar} from '../ui';
+import { HeroesRoutes } from "../heroes";
 
 export const AppRouter = () => {
   return (
     <>
-        <Navbar/>
         <Routes>
-            <Route path="marvel" element={<MarvelPage/>} />
-            <Route path="dc" element={<DcPage/>} />
-            
             <Route path="login" element={<LoginPage/>} />
-
-
-            <Route path="/" element={<Navigate to={"marvel"} />} />
+            {/* creamos una ruta que me lleve a la ruta de heroes */}
+            <Route path="/*" element={<HeroesRoutes/>} />
         </Routes>
 
     </>
