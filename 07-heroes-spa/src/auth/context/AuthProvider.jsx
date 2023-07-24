2107e1e0434aabdbf0464e5f5515bccb6fe96871
Adrian <adriferreiro.88@gmail.com>
2023-07-24 08:22:19 -0300

    212.- Mantener el usuario activo mientras no se cierre la sesión

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -6,23 +6,34 @@ import { authReducer } from "./authReducer"
 import { types } from "../types/types"
 
 
-const initialState = {
-    logged: false,
+// const initialState = {
+//     logged: false,
+// }
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    logged: !!user, //le ponemos doble negación entonces si el user existe va a ser true
+    //y si existe entonces pone el user
+    user: user,
+  }
 }
 
 export const AuthProvider = ({children}) => {
 
-  const [authState, dispatch] =  useReducer( authReducer, initialState );
+  const [authState, dispatch] =  useReducer( authReducer, {}, init );
 
   const login = (name = '') => {
     
+    const user = { id: 'ABC', name }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name,
-      }
+      payload: user
     }
+
+    localStorage.setItem('user', JSON.stringify( user ));
 
     dispatch(action);
   }
