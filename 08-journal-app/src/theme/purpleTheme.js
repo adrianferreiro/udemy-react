d9d595b0a9ad2025100e6f7fed8097c9c7610d36
Adrian <adriferreiro.88@gmail.com>
2023-07-25 10:59:31 -0300

    241.- MUI - Configuración con Vite

---
+++ 08-journal-app/src/theme/purpleTheme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+export const purpleTheme = createTheme({
+    //tiene un tema por defecto pero sobre escribimos lo que queremos
+    //por ejemplo la paleta
+    palette:{
+        primary: {
+            main: '#262254'
+        },
+        secondary:{
+            main: '#543884'
+        },
+        error: {
+            main: red.A400
+        }
+    }
+});
