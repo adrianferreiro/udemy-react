b103deb173b6bcd3280bb071faa6d5de3582b9db
Adrian <adriferreiro.88@gmail.com>
2023-07-28 12:25:35 -0300

    260.- Usar valores del storey despachar acciones

--- 09-toolkit-redux/src/App.jsx
+++ 09-toolkit-redux/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { increment } from './store/slice/counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //useSelector es un callback que tiene como primer argunmento el state y regreso del state la información que me interesa
+  const { counter } = useSelector(state => state.counter); //toma el valor del counter que tengo en el store
+  //toma el objeto completo del counter que puede tener muchos valores y desestructura el counter de counterSlice.js.
+  const dispatch = useDispatch(); //para despachar las acciones. En éste ejemplo en el click
 
   // Instalamos los paquetes para REDUX
   // npm install @reduxjs/toolkit
@@ -21,8 +26,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch( increment() ) }>
+          count is {counter}
         </button>  
       </div>
     </>
--- 09-toolkit-redux/src/store/store.js
+++ 09-toolkit-redux/src/store/store.js
@@ -6,3 +6,8 @@ export const store = configureStore({
         counter: counterSlice.reducer,
     },
 });
+
+//store: está toda la información que podemos acceder desde cualquier componente
+//usamos dos hooks para interactuar con nuestro store
+//useSelector: seleccionamos el estado que queremos ocupar
+//useDispatch: para despachar acciones
