bfdf963256647e4b933247312c2c429ca13ec17f
Adrian <adriferreiro.88@gmail.com>
2023-08-01 10:20:43 -0300

    268.- Consumir API mediante custom hook - RTK Query
    Api de prueba: lista de tareas completadas y pendientes

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,12 +1,34 @@
+import { useGetTodosQuery } from "./store/apis/todosApi";
+
 export const TodoApp = () => {
+
+
+    const { data: todos = [], isLoading } = useGetTodosQuery(); //data: todos dice que desestructura data y renombra a "todos"
+    // console.log(algo); //imprimimos para ver que es lo que podemos desestructurar (data, isLoading, isFetching)
+
   return (
         <>
         {/* RTK QUERY (REDUX TOOLKIT: Sirve para controlar las peticiónes http y optimizarlas) */}
             <h1>Todos - RTK Query</h1>
             <hr />
-            <h4>isLoading...</h4>
+            <h4>isLoading: { isLoading ? 'True' : 'False' } </h4>
 
-            <pre></pre>
+            <pre>...</pre>
+
+            <ul>
+
+                { todos.map ( todo => (
+                    <li key={todo.id} >
+                        <strong> { todo.completed ? 'Done' : 'Pending' } </strong>
+                        { todo.title }
+                    </li>
+                )
+
+                )
+
+                }
+
+            </ul>
 
             <button>Next Todo</button>
         </>
--- 09-toolkit-redux/src/store/store.js
+++ 09-toolkit-redux/src/store/store.js
@@ -1,12 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './slice/counter';
 import { pokemonSlice } from './slice/pokemon';
+import { todosApi } from './apis/todosApi';
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
         pokemons: pokemonSlice.reducer,
+
+        //usamos corchetes para decir que es una propiedad computada
+        //esto es el espacio en el que vamos a colocar los resultados de las query
+        [todosApi.reducerPath]: todosApi.reducer,
     },
+    //un middleware es una función que se ejecuta antes que otra
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat( todosApi.middleware )
 });
 
 //store: está toda la información que podemos acceder desde cualquier componente
