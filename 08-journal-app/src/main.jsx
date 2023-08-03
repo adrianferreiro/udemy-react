e8ae521cb4280111465ee0faf0282d3a99abafcd
Adrian <adriferreiro.88@gmail.com>
2023-08-03 08:03:07 -0300

    274-.75.- Configurando Redux y AuthSlice

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+//instalamos redux
+//https://redux-toolkit.js.org/tutorials/quick-start
+// npm install @reduxjs/toolkit react-redux
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
